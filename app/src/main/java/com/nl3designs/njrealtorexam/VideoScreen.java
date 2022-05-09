9eeec2f951da994a87070961e1dad26ea1d96a32
nl3appdev21 <56339045+nl3appdev21@users.noreply.github.com>
2022-05-08 23:08:23 -0400

    commit video screen changes

---
+++ app/src/main/java/com/nl3designs/njrealtorexam/VideoScreen.java
@@ -0,0 +1,50 @@
+package com.nl3designs.njrealtorexam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VideoScreen extends AppCompatActivity {
+
+    private Button btnBack2Menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.videoscreen);
+        btnBack2Menu = findViewById(R.id.btn_back2Menu);
+        TextView tv_title = findViewById(R.id.tv_video_title);
+        btnBack2Menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                back2Menu();
+            }
+        });
+
+        Bundle bundle = getIntent().getExtras();
+        String video = bundle.getString("video");
+        String title = bundle.getString("title");
+        tv_title.setText(title);
+        int resourceId = this.getResources().getIdentifier(video, "raw", this.getPackageName());
+        VideoView videoview = findViewById(R.id.video_player);
+        videoview.setVideoPath("android.resource://"+getPackageName()+"/"+resourceId);
+        MediaController mediaController = new MediaController( this);
+        mediaController.setAnchorView(videoview);
+        videoview.setMediaController(mediaController);
+    }
+
+    // TextView
+
+    private void back2Menu() {
+
+        Intent intent = new Intent(com.nl3designs.njrealtorexam.VideoScreen.this, MenuActivity.class);
+        startActivity(intent);
+    }
+}
+
